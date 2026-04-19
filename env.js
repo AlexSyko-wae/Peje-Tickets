81c2b72154641127e4967b18521d25e8d7376ac0
Alex Vargas <aralyvargas74@gmail.com>
2026-04-19 13:00:30 -0700

    Guardar cambios antes de hacer pull

--- env.js
+++ env.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 // Configuración global para producción.
 // Cambia esta URL por la dirección real de tu backend desplegado en Render.
 window.PEJE_API_BASE = "https://peje-tickets.onrender.com";
+=======
+// Configuración global para producción.
+// Cambia esta URL por la dirección real de tu backend desplegado en Render.
+window.PEJE_API_BASE = "https://peje-tickets.onrender.com";
+>>>>>>> 52f52df (Primer commit Peje Tickets)
